96708bcd26fce8ffcfc260338164b864aef01f5e
buğra uysal <uysalbugra134@gmail.com>
2025-06-10 23:37:05 +0300

    Finding the popularity of the words stock, dollar, gold in Türkiye with Google Trends API from Python

---
+++ ftrend.py
@@ -0,0 +1,14 @@
+from pytrends.request import TrendReq
+import pandas as pd
+
+#google a bağlan
+pytrends=TrendReq(hl='tr-TR- tz=360')
+#araştırılcak kelime
+resorch_word=['hisse','altın','dollar',""]
+
+
+""" trend verisini al   ve hangi zaman , aralık, ülke de  arama yapmak istediğini belirtirsin   / cat=0, kategori ıd si"""
+pytrends.build_payload(resorch_word, cat=0, timeframe='today 12-m', geo='TR')
+# zaman serisi verisini al (ilgi düzeyi zaman içerisinde)
+data=pytrends.interest_over_time()
+print(data.head())
