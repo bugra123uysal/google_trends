d67bbff073a836671919da224fdaee35e3770fd6
buğra uysal <uysalbugra134@gmail.com>
2025-06-11 00:28:49 +0300

    Data is transferred to an Excel file.

--- ftrend.py
+++ ftrend.py
@@ -4,11 +4,16 @@ import pandas as pd
 #google a bağlan
 pytrends=TrendReq(hl='tr-TR- tz=360')
 #araştırılcak kelime
-resorch_word=['hisse','altın','dollar',""]
+resorch_word=['hisse','altın','dollar','faiz']
+country=['TR','USA' ]
+for  cou in country:
 
+  """ trend verisini al   ve hangi zaman , aralık, ülke de,  arama yapmak istediğini belirtirsin   / cat=0, kategori ıd si"""
+  pytrends.build_payload(resorch_word, cat=0, timeframe='today 12-m', geo=cou)
+  # zaman serisi verisini al (ilgi düzeyi zaman içerisinde)
+  data=pytrends.interest_over_time()
+  print(data.head())
+  
+  aa=pd.DataFrame(data)
 
-""" trend verisini al   ve hangi zaman , aralık, ülke de  arama yapmak istediğini belirtirsin   / cat=0, kategori ıd si"""
-pytrends.build_payload(resorch_word, cat=0, timeframe='today 12-m', geo='TR')
-# zaman serisi verisini al (ilgi düzeyi zaman içerisinde)
-data=pytrends.interest_over_time()
-print(data.head())
+aa.to_excel("trends.xlsx", index=True)
